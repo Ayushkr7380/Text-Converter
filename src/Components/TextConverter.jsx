48cffd5dfadbb99d0e93733c148b9b68314c0c19
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-02-17 19:27:53 +0530

    main

--- src/Components/TextConverter.jsx
+++ src/Components/TextConverter.jsx
@@ -60,30 +60,34 @@ const TextConverter = (props) => {
 
         <nav className={`navbar navbar-expand-lg  ${darkmode === 'dark'?'bg-dark':'bg-primary'}`}  style={{
           color : darkmode === 'dark'?'white':'' }}  >
-    <div className="container-fluid" >
-    <a className="navbar-brand" href="#" style={{
-          color : darkmode === 'dark'?'white':'' }}>Text-Converter</a>
-    {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button> */}
-    <div className="collapse navbar-collapse" id="navbarSupportedContent" >
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#" style={{
-          color : darkmode === 'dark'?'white':'' }}>Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#" style={{
-          color : darkmode === 'dark'?'white':'' }}>Link</a>
-        </li>
-      </ul>
-      <div className="form-check form-switch">
-        <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"  onClick={enabledarkmode}/>
-        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DarkMode</label>
-      </div>
-    </div>
-  </div>
-      </nav>
+              <div className="container-fluid" >
+                <a className="navbar-brand" href="#" style={{
+                    color : darkmode === 'dark'?'white':'' }}>Text-Converter</a>
+                        <button style={{
+                            backgroundColor : darkmode === 'dark'?'white':'' }}className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" >
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+                    <li className="nav-item">
+                      <a className="nav-link active" aria-current="page" href="#" style={{
+                        color : darkmode === 'dark'?'white':'' }}>Home</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#" style={{
+                      color : darkmode === 'dark'?'white':'' }}>Link</a>
+                    </li>
+                  </ul>
+                  <div className="form-check form-switch">
+                      <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"  onClick={enabledarkmode}/>
+                      <label className="form-check-label" htmlFor="flexSwitchCheckDefault">DarkMode</label>
+                  </div>
+                </div>
+              </div>
+        </nav>
+  
+
+
         <div className="main" style={
           {color: darkmode === 'dark'?'white':'black'}}>
             <h1>{props.title1}</h1>
